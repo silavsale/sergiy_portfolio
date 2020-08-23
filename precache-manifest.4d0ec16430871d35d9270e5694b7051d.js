df7f7c4d0eb61cbda3001ae802bd7add45bfda33
silavsale <silavsale@gmail.com>
2020-08-23 15:31:45 -0400

    Updates

---
+++ precache-manifest.4d0ec16430871d35d9270e5694b7051d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d64f1477234795c78c23e45d08f4c320",
+    "url": "/sergiy_portfolio/index.html"
+  },
+  {
+    "revision": "5f7b03da4ee65cde1dcb",
+    "url": "/sergiy_portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3c92efb7109d7052a05b",
+    "url": "/sergiy_portfolio/static/js/2.d7ad4f8a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sergiy_portfolio/static/js/2.d7ad4f8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f7b03da4ee65cde1dcb",
+    "url": "/sergiy_portfolio/static/js/main.1ef3da8a.chunk.js"
+  },
+  {
+    "revision": "e505a1b161803a39747b",
+    "url": "/sergiy_portfolio/static/js/runtime-main.efc07c48.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sergiy_portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
